e470c24d096df13c780db7881620cf9c23e63b74
RC0026850 <RC0026850@CRO-ICF07-09>
2017-10-11 15:58:39 +0100

    ...

--- Moving Picture 2/Form1.cs
+++ Moving Picture 2/Form1.cs
@@ -26,5 +26,13 @@ namespace Moving_Picture_2
              
             
         }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y + 50);
+            await Task.Delay(500);
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 50, pictureBox1.Location.Y - 50);
+            await Task.Delay(500);
+        } 
     }
 }
