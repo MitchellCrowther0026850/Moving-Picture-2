d8abde75764670d6446e9b6b893b9003f6c37987
RC0026850 <RC0026850@CRO-ICF07-09>
2017-11-22 13:24:02 +0000

    ttp

--- Moving Picture 2/Form1.cs
+++ Moving Picture 2/Form1.cs
@@ -33,6 +33,51 @@ namespace Moving_Picture_2
             await Task.Delay(500);
             pictureBox1.Location = new Point(pictureBox1.Location.X + 50, pictureBox1.Location.Y - 50);
             await Task.Delay(500);
-        } 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 30, pictureBox1.Location.Y - 30);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 100, pictureBox1.Location.Y - 0);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 100, pictureBox1.Location.Y - 0);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 0, pictureBox1.Location.Y + 50);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 0, pictureBox1.Location.Y - 50);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y - 50);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 50, pictureBox1.Location.Y + 50);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y + 50);      
+
+        }
     }
 }
